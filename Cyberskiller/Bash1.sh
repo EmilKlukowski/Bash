431d70dd6096ee3a94098c3c3a26e0c578ff39ee
Emil Klukowski <121125273+EmilKlukowski@users.noreply.github.com>
2023-04-13 21:12:59 +0200

    Update Bash1.sh

--- Cyberskiller/Bash1.sh
+++ Cyberskiller/Bash1.sh
@@ -1,1 +1,19 @@
- 
+#Skrypt przy wykonaniu dostanie nieokreśloną liczbę argumentów typu string.
+#Program ma dla każdego podanego wyrazu wyświetlić na ekranie jego długość (w nowej linii).
+
+#Przykładowe wartości argumentów:
+
+#ala ma kota
+
+#Przykładowe wyjście:
+
+#3
+#2
+#4
+
+#!/bin/bash
+
+for word in "$@"; do
+    len=${#word}
+    echo "$len"
+done
